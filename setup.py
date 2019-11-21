c79ad80c34a1bf64f11f7b5bfe95efaa3c4bc8a1
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2019-11-22 00:45:27 +0100

    [Packaging] Fixed the version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name = 'OpenTrepWrapper',
-    version = '0.07.4-1',
+    version = '0.7.4.post1',
     author = 'Alex Prengere',
     author_email = 'alex.prengere@gmail.com',
     url = 'https://github.com/trep/wrapper',
